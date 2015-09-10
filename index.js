18df77bf0000d5e1083e15522c778b6cddea7b79
StoraH <hjalmar.skoldefors@wunderkraut.com>
2015-09-10 10:46:19 +0200

    Make use of states so we can update the counter numbers.

--- index.js
+++ index.js
@@ -4,38 +4,67 @@ var ease = require('ease-component');
 
 var Counter = React.createClass({
   getInitialState: function() {
-    return { value: this.props.begin };
+    return {
+      value: this.props.begin,
+      time: this.props.time,
+      begin: this.props.begin,
+      end: this.props.end,
+      easing: this.props.easing,
+      start:  Date.now(),
+      stop: false
+    };
   },
 
   componentDidMount: function() {
-    this.start = Date.now();
     raf(this.animate);
   },
 
   animate: function() {
-    if (this.stop) return;
+    if (this.state.stop) return;
 
     raf(this.animate);
     this.draw()
   },
 
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      time: nextProps.time,
+      begin: nextProps.begin,
+      end: nextProps.end,
+      easing: nextProps.easing,
+      start: Date.now(),
+      stop: false
+    });
+
+    raf(this.animate);
+    this.draw();
+  },
+
   draw: function() {
     if (!this.isMounted()) return;
 
-    var time = this.props.time;
-    var begin = this.props.begin;
-    var end = this.props.end;
-    var easing = this.props.easing;
+    var time = this.state.time;
+    var begin = this.state.begin;
+    var end = this.state.end;
+    var easing = this.state.easing;
 
     easing = easing && easing in ease ? easing : 'outCube';
-    var now = Date.now()
-    if (now - this.start >= time) this.stop = true;
-    var percentage = (now - this.start) / time;
+
+    var now = Date.now();
+    if (now - this.state.start >= time) {
+      this.setState({
+        stop: true
+      });
+    }
+
+    var percentage = (now - this.state.start) / time;
     percentage = percentage > 1 ? 1 : percentage;
     var easeVal = ease[easing](percentage);
     var val = begin + (end - begin) * easeVal;
 
-    this.setState({ value: val });
+    this.setState({
+      value: val
+    });
   },
 
   render: function() {
